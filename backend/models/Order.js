139688804f9327ccd10ef867b7ce6812045d318e
Dipika K.C <dipikak0323@gmail.com>
2026-03-23 00:03:13 +0545

    order schema

---
+++ backend/models/Order.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+      },
+    ],
+    totalPrice: Number,
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
+
+ 
