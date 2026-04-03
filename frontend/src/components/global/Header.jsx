c41b22f34ee6a657c74ede1fb6506ffaaad8c8ba
Dipika K.C <dipikak0323@gmail.com>
2026-04-03 23:36:42 +0545

    header ui

--- frontend/src/components/global/Header.jsx
+++ frontend/src/components/global/Header.jsx
@@ -1,9 +1,30 @@
+import { HeartIcon, SearchIcon, ShoppingCartIcon, User2Icon } from 'lucide-react'
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
-  return (
-    <div>
-      this is a header
-    </div>
-  )
+    return (
+        <header className='w-full h-fit py-4 px-20 flex justify-end items-center space-x-100 border-2'>
+            <div className='items-center space-x-10'>
+                <NavLink>Home</NavLink>
+                <NavLink>Shop</NavLink>
+                <NavLink>About</NavLink>
+                <NavLink>Contact</NavLink>
+            </div>
+            <div className='flex items-center space-x-6'>
+                <NavLink>
+                    <User2Icon size={20} />
+                </NavLink>
+                <div>
+                    <SearchIcon size={20} />
+                </div>
+                <NavLink>
+                    <HeartIcon size={20} />
+                </NavLink>
+                <NavLink>
+                    <ShoppingCartIcon size={20} />
+                </NavLink>
+            </div>
+        </header>
+    )
 }
