8c736c251aef92b81860a9e54867cadef886fe1f
Dipika K.C <dipikak0323@gmail.com>
2026-05-13 01:07:36 +0545

    google authetication debug

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,34 +8,11 @@ const router = express.Router();
 router.route('/auth/google')
     .get(passport.authenticate("google", { scope: ["profile", "email"] }))
 
-// router.route('/auth/google/callback')
-//     .get(
-//         passport.authenticate("google", { failureRedirect: "/auth/failure" }),
-//         googleAuthSuccess
-//     );
-
 router.route('/auth/google/callback')
     .get(
-        (req, res, next) => {
-            console.log("Callback route reached");
-            next();
-        },
-        passport.authenticate("google", (err, user, info) => {
-            if (err) {
-                console.log("Passport error:", err);
-                return res.status(500).send("Authentication error");
-            }
-
-            if (!user) {
-                console.log("No user returned", info);
-                return res.redirect("/auth/failure");
-            }
-
-            req.user = user;
-            return googleAuthSuccess(req, res);
-        })
+        passport.authenticate("google", { failureRedirect: "/auth/failure" }),
+        googleAuthSuccess
     );
-
 
 router.route('/auth/failure')
     .get(googleAuthFailure);
